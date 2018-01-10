4a7dc591c8462443a50a83e8cd556e65d9a0797b
unknown <adambleclair@gmail.com>
2018-01-09 20:58:13 -0800

    popout handle url ending in /

--- public/js/board.js
+++ public/js/board.js
@@ -121,5 +121,5 @@ function removeStartButton() {
 }
 
 function popoutBoard() {
-	window.open(window.location.href + '/popout', '_blank', 'width=700,height=' + (bingoSize * 128 + 2));
+	window.open(window.location.href + (window.location.href[window.location.href.length - 1] == '/' ? '' : '/') + 'popout', '_blank', 'width=700,height=' + (bingoSize * 128 + 2));
 }
