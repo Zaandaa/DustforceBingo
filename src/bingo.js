f298773b99e8cf0c63a91bea1b40270dd2d1eac9
Corey Byrne <coreyrbyrne@gmail.com>
2018-01-20 13:05:39 -0800

    fix to resetting board

--- src/bingo.js
+++ src/bingo.js
@@ -454,7 +454,7 @@ var Bingo = function(session, ruleset) {
 		}
 
 		// new goals
-		self.goals = goal.makeGoals(ruleset);
+		self.goals = goal.makeGoals(ruleset, self.possibleBingos);
 
 		self.session.resetBingo();
 	};
