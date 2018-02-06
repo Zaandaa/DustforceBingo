397f3a041775dfa3ab0fd3d6120a68072b0238a3
unknown <adambleclair@gmail.com>
2018-02-05 22:24:37 -0800

    proxy

--- src/session.js
+++ src/session.js
@@ -79,7 +79,7 @@ function build(io) {
 	function getUserJson(name, lambda, success) {
 		var number = parseInt(name) 
 		
-		var url = 'http://df.hitboxteam.com/backend6/userSearch.php?' + querystring.stringify({
+		var url = 'http://proxy.dustkid.com/backend6/userSearch.php?' + querystring.stringify({
 			'q':name,
 			'max':50,
 		});
@@ -88,7 +88,7 @@ function build(io) {
 			if (json.length == 0) {
 				if (isNaN(number)) 
 					return lambda(true, false, `<strong>User ${name} not found!</strong>`);
-				var url = 'http://df.hitboxteam.com/backend6/userSearch.php?' + querystring.stringify({
+				var url = 'http://proxy.dustkid.com/backend6/userSearch.php?' + querystring.stringify({
 					'userid':name
 				});
 				requestWrapper(url, lambda, function(json) {
