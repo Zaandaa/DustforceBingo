a20749c90546bc89c33cdd61d69002d6be98d3c2
unknown <adambleclair@gmail.com>
2021-11-21 18:37:41 -0500

    rando link on session page

--- public/js/board.js
+++ public/js/board.js
@@ -711,6 +711,10 @@ function addCopyUrl() {
 	$("#under_players").empty();
 	if (!bingoStarted)
 		$("#under_players").append("<p>Copy <a href=''>this url</a> to invite more players</p>");
+	if (ruleset.gametype == "bingo" && ruleset.rando)
+		$("#under_players").append("<p><a href='" + ruleset.rando_link + "'>Randomizer link</a></p>");
+	else if (ruleset.gametype == "64" && ruleset.rando64)
+		$("#under_players").append("<p><a href='" + ruleset.rando_link64 + "'>Randomizer link</a></p>");
 }
 
 function resetBingo() {
